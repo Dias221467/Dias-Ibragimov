653063b0e66e54e6eeb635ed8a323526e3f45eed
Диас <lbrjyz05@mail.ru>
2023-04-09 15:47:25 +0600

    2

--- Main.java
+++ Main.java
@@ -1,28 +1,44 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Choose which task you want to try:");
+        System.out.println("Choose which option you want to try:");
+        System.out.println("1 - minimum\n2 - average\n3- prime numbers\n");
         Scanner scanner = new Scanner(System.in);
         int option = scanner.nextInt();
         switch (option){
             case 1:
                 System.out.println("Write the amount of numbers of array:");
-                int n = scanner.nextInt();
-                int[] arr = new int[n];
-                for (int i = 0; i<n; i++){
+                int n1 = scanner.nextInt();
+                int[] arr = new int[n1];
+                for (int i = 0; i<n1; i++){
                     arr[i] = scanner.nextInt();
                 }
-                System.out.println(find_minimum(n, arr));
+                System.out.println(find_minimum(n1, arr));
                 break;
+            case 2:
+                System.out.println("Write the amount of numbers of array:");
+                int n2 = scanner.nextInt();
+                int[] arr2 = new int[n2];
+                for (int i2 = 0; i2<n2; i2++){
+                    arr2[i2] = scanner.nextInt();
+                }
+
         }
     }
-    public static int find_minimum(int n, int[] arr){
+    public static int find_minimum(int n1, int[] arr){
         int min = arr[0];
-        for (int i = 1; i < n; i++) {
+        for (int i = 1; i < n1; i++) {
             if (arr[i] < min) {
                 min = arr[i];
             }
         }
         return min;
+    }
+    public static int find_average(int n2, int[] arr2){
+        int total = 0;
+        for(int i = 0; i<n2; i++){
+            total= arr2[i] + total;
+        }
+        return total;
     }
 }
